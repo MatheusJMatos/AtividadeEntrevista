663646d9bc450de75add705c8b11bc87c5470917
MatheusJMatos <madsaraj@hotmail.com>
2021-02-19 23:48:19 -0300

    First commit
    Primeiro comit

---
+++ FI.AtividadeEntrevista/DML/Beneficiario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.DML
+{
+    public class Beneficiario
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// CPF
+        /// </summary>
+        public string CPF { get; set; }
+
+        /// <summary>
+        /// Cliente
+        /// </summary>
+        public Cliente Cliente { get; set; }
+
+
+    }
+}
